6de859e10b1e999f321d16dd77241e648ad906e2
karrey <109669003+karreycodes@users.noreply.github.com>
2023-02-12 18:49:19 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""Implements the user's model"""
+"""Module for User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    Inherits from the BaseModel class and add user's functionalities
-
-    Args:
-        email (str): the email of the user
-        password (str): the password of the user
-        first_name (str): the first name of the user
-        last_name (str): the last name of the user
-    """
+    """Class representing a User."""
     email = ""
     password = ""
     first_name = ""
